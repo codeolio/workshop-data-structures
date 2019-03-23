f74b10dbdfa8c88e9372f5200e3c5f6e9e2832a0
david-dest01 <david.dest01@gmail.com>
2019-03-23 17:45:03 -0400

    notess for dictionary

---
+++ src/6_dictionary.js
@@ -0,0 +1,46 @@
+/**
+ * ********************************************************************************************************************************************
+ * @module  Dictionary
+ * @author  david-dest01
+ * @date    03/23/19
+ * @description Begin challenge below. 
+ *              Details: For more information on Dictionary, please review section ____ within the README
+ *              Testing Your Code: 
+ *                - To test code, run npm test in terminal and complete everything under Dictionary data structure
+ *                - For extensions, remove 'x' before describe on line ____ in testing file then save.
+ * ********************************************************************************************************************************************
+ */
+
+// dictionary 
+
+// main class
+// - to string function
+// - table (object)
+
+
+// methods
+
+// set(key, value) - method adds a new element to the dictionary. if th ekey already exists, the existing values will be overwritten with the new one
+
+// remove(key) - the method removes the value from the dictionary using the key as the search parameter
+
+// hashKey(key) - method returns true if the key exists in the dictionary and false if not
+
+// get(key) - returns a specifc value from the dictionary using the key as the search parameter 
+
+// clear() removes all the values from the dictionary
+
+// size() returns how many values the dictionary contains, similar the the length property of an array
+
+// isEmpty returns true if the size equals zero and false if otherwise 
+
+// keys -> returns and array of all the keys the dictionary contains
+
+// values -> returns an array of all the values of the dictionary. 
+
+// keyValues() -> returns an array of all value pairs [key, value] of the dictionary
+
+
+/********************************************************************************************************************************/
+/*                                             BONUS - EXTENSION METHODS                                                        */
+/********************************************************************************************************************************/
