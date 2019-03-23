47b511127e56fda4096d418518283623b3c91a21
david-dest01 <david.dest01@gmail.com>
2019-03-23 17:45:30 -0400

    intitial setup for HT

---
+++ src/7_hash_table.js
@@ -0,0 +1,39 @@
+/**
+ * ********************************************************************************************************************************************
+ * @module  HashTable
+ * @author  david-dest01
+ * @date    03/23/19
+ * @description Begin challenge below. 
+ *              Details: For more information on HashTable, please review section ____ within the README
+ *              Testing Your Code: 
+ *                - To test code, run npm test in terminal and complete everything under HashTable data structure
+ *                - For extensions, remove 'x' before describe on line ____ in testing file then save.
+ * ********************************************************************************************************************************************
+ */
+
+/* uncomment below to use LinkedList data structure for collision prevention */
+// const { LinkedList, Node } = require('./4_linked_list.js');
+
+function HashTable (size) {
+ // set array up based on size
+};
+
+function hashingFunction(key, size) {
+  // logic for making hash function
+}
+
+// put
+
+// get 
+
+// remove
+
+// isEmpty
+
+// keys
+
+// contains
+
+/********************************************************************************************************************************/
+/*                                             BONUS - EXTENSION METHODS                                                        */
+/********************************************************************************************************************************/
