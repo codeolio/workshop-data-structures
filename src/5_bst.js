643daede0a5fd24b77b1d63377810f4011b00654
david-dest01 <david.dest01@gmail.com>
2019-03-25 17:40:42 -0400

    initial bst comments

--- src/5_bst.js
+++ src/5_bst.js
@@ -1,15 +1,13 @@
-/**
- * ********************************************************************************************************************************************
+/**********************************************************************************************************************************************
  * @module  BinarySearchTree
  * @author  david-dest01
  * @date    03/23/19
- * @description Begin challenge below. 
+ * @description Begin Binary Search Tree challenge below. 
  *              Details: For more information on BinarySearchTree, please review section ____ within the README
  *              Testing Your Code: 
  *                - To test code, run npm test in terminal and complete everything under BinarySearchTree data structure
  *                - For extensions, remove 'x' before describe on line ____ in testing file then save.
- * ********************************************************************************************************************************************
- */
+ * ********************************************************************************************************************************************/
 
 function BinarySearchTree (value) {
   this.value = value;
@@ -17,28 +15,52 @@ function BinarySearchTree (value) {
   this.right = null;
 }
 
-// insert(key) - inserts a new key in the tre
+/* CHALLENGE 1
+  * method: insert
+  * description: insert a new value in the tree
+    - insert a new value in the binary search tree
+*/
 BinarySearchTree.prototype.insert = function (value) {
   // insert code here
 };
 
-// search(key) - searches for the key in the tree and returns true if it exists and false if the node does not
+/* CHALLENGE 2
+  * method: search
+  * description: searches for the inputed value in the tree and returns true if it exists and false if the node does not
+*/
 BinarySearchTree.prototype.search = function (value) {
   // insert code here
 };
 
-// inOrderTraverse() visits all nodes of the tree using in-order traverse
+/* CHALLENGE 3
+  * method: inOrderTraverse
+  * description: visits all nodes of the tree from smallest (minimum value to largest (maximum value)
+*/
 BinarySearchTree.prototype.inOrderTraverse = function (callback) {
   // insert code here
 };
 
-// preOrderTraverse() visits all nodes of the tree using pre-order traverse
+/* CHALLENGE 4
+  * method: preOrderTraverse
+  * description: visits all nodes from the root most node down to leaf nodes, starting on the left side of the tree then the right side of the tree
+*/
 BinarySearchTree.prototype.preOrderTraverse = function (callback) {
   // insert code here
 };
 
-// postOrderTraverse() vists all nodes of the tree using post-order traverse
+/* CHALLENGE 5
+  * method: postOrderTraverse
+  * description: visits all nodes from the leaf nodes up to the root node, starting at the smallest node value
+*/
 BinarySearchTree.prototype.postOrderTraverse = function (callback) {
+  // insert code here
+};
+
+/* CHALLENGE 6
+  * method: breadthTraversal
+  * description: visits each node by level, starting at the zeroith level (root node), and working its way down to nth level (leaf nodes)
+*/
+BinarySearchTree.prototype.breadthTraversal = function (callback) {
   // insert code here
 };
 
@@ -46,17 +68,27 @@ BinarySearchTree.prototype.postOrderTraverse = function (callback) {
 /*                                             BONUS - EXTENSION METHODS                                                        */
 /********************************************************************************************************************************/
 
-// min - returns the minimum value/key in the tree
+/* CHALLENGE 7
+  * method: min
+  * description: returns the smallest value in tree
+*/ 
 BinarySearchTree.prototype.min = function () {
   // insert code here
 };
 
 // max - method returns the maximum value/key in the tree
+/* CHALLENGE 8
+  * method: max
+  * description: returns the largest value in tree
+*/ 
 BinarySearchTree.prototype.max = function () {
   // insert code here
 };
 
-// remove(key) removes the key from the tree
+/* CHALLENGE 9
+  * method: remove
+  * description: removes value from tree, does NOT effect children values
+*/ 
 BinarySearchTree.prototype.remove = function (value) {
   // insert code here
 };
