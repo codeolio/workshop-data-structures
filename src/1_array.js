37bcf4c1312048fd09cdddfa82d5cf3468e24717
david-dest01 <david.dest01@gmail.com>
2019-03-25 17:37:37 -0400

    array comments updated

--- src/1_array.js
+++ src/1_array.js
@@ -1,107 +1,112 @@
-/**
- * ********************************************************************************************************************************************
+/**********************************************************************************************************************************************
  * @module  Erray
  * @author  david-dest01
  * @date    03/23/19
- * @description Begin challenge below. 
+ * @description Begin Erray (Array Data Structure) challenge below. 
  *              Details: For more information on Array, please review section ____ within the README
  *              Testing Your Code: 
  *                - To test code, run npm test in terminal and complete everything under Array data structure
  *                - For extensions, remove 'x' before describe on line ___ in testing file then save.
- * ********************************************************************************************************************************************
- */
+ **********************************************************************************************************************************************/
 
 function Erray () {
   this.contents = {};
   this.length = 0;
 }
 
-/* X
-  CHALLENGE 1
+/* CHALLENGE 1
   * method: push
-  * description: adds a new elemetn to the end of the Erray
+  * description:
+    - the push method adds a new element to the end of the Erray
+    - updates length
   * example 1 - single value:
-    - queue before: 
-    - input:
-    - queue after: 
+    - array before: { 0: 'A' }
+    - input: push('B')
+    - array after: { 0: 'A', 1: 'B' }
 
-    BONUS
-    example 2 - multiple values: 
-    - queue before: 
-    - input: 
-    - queue after: 
+    BONUS - adding multiple values
+  * example 2 - multiple values: 
+    - array before: { 0: 'A' }
+    - input: push('B', 'C', 'D')
+    - array after: { 0: 'A', 1: 'B', 2: 'C', 3: 'D' }
 */
 Erray.prototype.push = function (value) {
   // insert code here
 };
 
-/* X
-  CHALLENGE 2
+/* CHALLENGE 2
   * method: pop
-  * description: removes and returns the last element from the Erray
-  * example 1:
-    - queue before: { '0': 'A', '1': 'B', '2': 'C' }
-    - output: 'A'
-    - queue after: { '0': 'B', '1': 'C' }
+  * description:
+    - removes and returns the last element from the Erray
+    - updates length
+  * example:
+    - array before: { '0': 'A', '1': 'B', '2': 'C' }
+    - output: 
+    - array before: { '0': 'A', '1': 'B' }
 */
 Erray.prototype.pop = function () {
   // insert code here
 };
 
-/* X
-  CHALLENGE 3
+/* CHALLENGE 3
   * method: shift
-  * description: adds a new element to the beginning of the Erray
+  * description:
+    - adds a new element to the beginning of the Erray
+    - updates length
   * example 1 - single value:
-    - queue before: 
-    - input:
-    - queue after: 
+    - array before: { 0: 'A' }
+    - input: shift('B')
+    - array after: { 0: 'B', 1: 'A' }
 
-  BONUS
-  * description: refactor to add multiple elements to the beginning of Erray
+    BONUS - adding multiple values
   * example 2 - multiple values: 
-    - queue before: 
-    - input: 
-    - queue after: 
+    - array before: { 0: 'A' }
+    - input: shift('B', 'C', 'D')
+    - array after: { 0: 'B', 1: 'C', 2: 'D', 3: 'A' }
 */
 Erray.prototype.shift = function (value) {
   // insert code here
 };
 
-/* X
-  CHALLENGE 4
+/* CHALLENGE 4
   * method: unshift
-  * description: removes the first element from the beginning of the Erray
-  * example 1:
-    - stack before:
-    - output: 'C'
-    - stack after: 
+  * description:
+    - removes the first element from the beginning of the Erray
+    - updates length
+  * example:
+    - array before: { 0: 'A',  1: 'B', 2: 'C' }
+    - input: unshift()
+    - array after: { 0: 'B', 1: 'C' }
 */ 
 Erray.prototype.unshift - function () {
   // insert code here
 };
 
-/* X
-  CHALLENGE 5
+/* CHALLENGE 5
   * method: indexOf
   * description: returns the position (index) of the first element in the Erray that matches the input value
   * example 1:
-    - stack before:
-    - output: 'C'
-    - stack after: 
+    - array: { 0: 'A', 1: 'B', 2: 'C', 3: 'B' }
+    - input: indexOf('B')
+    - output: 1
+  * example 2:
+    - input: indexOf('D')
+    - output: -1
 */ 
 Erray.prototype.indexOf = function (value) {
   // insert code here
 };
 
-/* X
-  CHALLENGE 6
+/* CHALLENGE 6
   * method: lastIndexOf
   * description: returns the position (index) of the last item in the Erray that matches the input value
   * example 1:
-    - stack before:
-    - output: 'C'
-    - stack after: 
+    - array: { 0: 'A', 1: 'B', 2: 'C', 3: 'B' }
+    - input: indexOf('B')
+    - output: 3
+  * example 2:
+    - input: indexOf('D')
+    - output: -1
 */ 
 Erray.prototype.lastIndexOf = function (value) {
   // insert code here
